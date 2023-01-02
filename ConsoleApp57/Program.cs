a644fea77180d9ebde493f4df35929ba78e172af
huseynManaf <huseynim@code.edu.az>
2023-01-02 14:06:05 -0800

    init

---
+++ ConsoleApp57/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Schema;
+
+namespace ConsoleApp57
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Massivin uzunlugun daxil et:");
+            int n= int.Parse(Console.ReadLine());
+            int sum = 0;
+            
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++)
+
+            {
+                Console.Write($"arr{i}+{i}=");
+                arr[i] = int.Parse(Console.ReadLine());
+
+
+            }
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] < 0) ;
+                sum += arr[i];
+                
+            }
+            Console.WriteLine("cem="+sum+" " + say=)
+        }    
+    }
+}
