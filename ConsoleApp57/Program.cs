bef609326f94cfe693e4cf144d90efce42779a30
huseynManaf <huseynim@code.edu.az>
2023-01-04 10:41:59 -0800

    init

--- ConsoleApp57/Program.cs
+++ ConsoleApp57/Program.cs
@@ -1,5 +1,7 @@
-﻿using System.ComponentModel;
+﻿using System;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
 using System.Xml.Schema;
 
 namespace ConsoleApp57
@@ -8,27 +10,130 @@ namespace ConsoleApp57
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Massivin uzunlugun daxil et:");
-            int n= int.Parse(Console.ReadLine());
-            int sum = 0;
-            
-            int[] arr = new int[n];
-            for (int i = 0; i < n; i++)
 
-            {
-                Console.Write($"arr{i}+{i}=");
-                arr[i] = int.Parse(Console.ReadLine());
+            #region task 1
 
 
-            }
+            //int count = 0;
+            //int sum = 0;
+            //int[] numbers = { 5, -12, 9, -13, -17, 23, -98, 0, -987 };
+            //for (int i = 0; i < numbers.Length; i++)
+            //{
+            //    sum += numbers[i];
+            //    count++;
 
-            for (int i = 0; i < arr.Length; i++)
-            {
-                if (arr[i] < 0) ;
-                sum += arr[i];
-                
-            }
-            Console.WriteLine("cem="+sum+" " + say=)
-        }    
-    }
+            //}
+            //Console.WriteLine(count);
+            //Console.WriteLine(sum);
+            #endregion
+
+            #region task 2
+            //int count = 0;
+            //int sum = 0;
+            //int[] numbers = { -5, 15, -20, 21, -35 ,23,-47, -60, 23, 27, -29, 31, -33, };
+            //for (int i = 0; i < numbers.Length; i++)
+            //{
+            //    if (numbers[i] % 5==0) ;
+            //    sum += numbers[i];
+            //    count++;
+            //}
+            //Console.WriteLine(count);
+            //Console.WriteLine(sum);
+
+            #endregion
+            #region task3
+            //int count=0;
+            //int sum=0;
+            //int[] numbers = { 1, 2, 4, 6, 7, 9, 20, 13, 27, 16, 8 };
+            //for (int i = 0; i < numbers.Length; i++)
+            //{
+            //    if (numbers[i] % 2 == 0 & (i - 1) / 2 == 0) ;
+            //    sum += numbers[i];
+            //    count++;
+            //}
+            //Console.WriteLine($"Musbet ve tek ededlerin cemi:{sum}");
+            //Console.WriteLine($"Musbet ve tek ededlerin sayi {count}");
+
+            #endregion
+            #region task 4 baxilacaq
+            //int count = 0;
+            //int sum = 0;
+            //int[] numbers = { -10, 2, 24, 6, -7, 28, 20, 13, 27, 45, 8, -76 , 5 };
+            //for (int i = 0; i < numbers.Length; i++)
+            //{
+
+            //    sum += numbers[i];
+            //    count++;
+            //}
+            //Console.WriteLine($"cut yerlerde olan cut ededin  cemi:{sum}");
+            //Console.WriteLine($"cut yerlerde olan cut ededin sayi :{count}");
+
+            #endregion
+            #region 5
+            //int[] numbers = { 1, 3, 4, 6, 8, 9, 12, 16, 21, 25 };
+            //int sum = 0;
+            //    int count = 0;
+            //    for (int i = 0; i < numbers.Length; i++)
+            //{
+            //   if (i % 2 == 0 && numbers[i] %2 !=0)
+
+            //    { 
+            //    sum += numbers[i];
+            //    count++;
+            //    }
+            // Console.WriteLine($"tek yerlerde olan cut elementleriN cemi {sum}");
+            //    Console.WriteLine($"tek yerlerde olan cut elementleriN sayi {count}");
+
+
+            //}
+
+            #endregion
+            #region 6
+            //int[] numbers = { 1, 3, 6, 7, 8, 19, 12, 23, 21, 25 };
+            //int sum = 0;
+            //int count = 0;
+            //for (int i = 0; i < numbers.Length; i++)
+            //{
+            //    if (numbers[i] % 2 != 0 && i % 2 == 0)
+
+
+            //    {
+            //        sum += numbers[i];
+            //        count++;
+            //    }
+            //    Console.WriteLine($"tek yerlerde olan cut elementleriN cemi {sum}");
+            //    Console.WriteLine($"tek yerlerde olan cut elementleriN sayi {count}");
+            #endregion
+            #region  task 7
+            //int[] numbers = { 2, 4, 5, 6, 7, 9, 12, 14, 16, 17, 19, 21, 28, 56 };
+            //int count = 0;
+            //for (int i = 1; i < numbers.Length; i++)
+            //{
+            //    if (numbers[i] % 7 == 1 || numbers[i] % 7 == 2 || numbers[i] % 7 == 5) ;
+            //    count++;
+            //}
+            //Console.WriteLine($"7 e bolunende qaliqda 1 2 ve 5 qalan ededler={count}");
+
+            #endregion
+            #region task 8
+            //int[] numbers = { 7, 3, 73,7, 8, 87, 12, 23, 21, 19 };
+            //int sum = 0;
+            //int count = 0;
+            //for (int i = 0; i < numbers.Length; i++)
+            //{
+            //    if (numbers[i] % 2 != 0 && i % 2 == 0)
+
+
+            //    {
+            //        sum += numbers[i * i];
+            //        count++;
+            //    }
+            //    Console.WriteLine($"tek yerlerde olan cut elementleriN cemi {sum}");
+            //    Console.WriteLine($"tek yerlerde olan cut elementleriN sayi {count}");
+           //
+            #endregion
+
+        }
+    }    
+    
 }
